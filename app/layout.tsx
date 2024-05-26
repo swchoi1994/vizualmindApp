1aa8bceb610d3c718af90fe76510f8a2d8e3ce81
Seongwoo Choi <swchoi1994@gmail.com>
2024-05-26 20:15:06 +0900

    deleting sidebar for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
-          <Sidebar />
+          {/* <Sidebar /> */}
           <Footer />
           <Toaster />
         </ThemeProvider>
